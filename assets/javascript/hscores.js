31cfa916f3cad907e17b07355166552015de070b
Richard-Au1 <136401843+Richard-Au1@users.noreply.github.com>
2023-07-27 20:48:50 -0600

    finished view highscore

--- assets/javascript/hscores.js
+++ assets/javascript/hscores.js
@@ -3,7 +3,7 @@ var scoresBtn = document.querySelector("#view-high-scores");
 // Rank previous scores in order by retrieving scores from localStorage
 
 function printHighscores() {
-    var highscores = JSON.parse(window.localStorage.getItem("high scores")) || [];
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
     highscores.sort(function(a, b) {
       return b.score - a.score;
     });
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,40 @@
 var questions = [
-    {
-        prompt: "Inside which HTML element do we put the JavaScript?",
-        options: ["<javascript>", "<js>", "<script>", "<scripting>"],
-        answer: "<script>"
-    },
+{
 
-    {
-        prompt: "How do you call a function named myFunction?",
-        options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction"],
-        answer: "myFunction()"
-    },
+    prompt: "Which language is a coding language?",
+    options: ["<javascript>", "<japanese>", "<penguin>", "<English>"],
+    answer: "<javascript>"
+},
 
-    {
-        prompt: "How does a for loop start?",
-        options: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", " for (i <= 5; i++)"],
-        answer: "for (i = 0; i <= 5; i++)"
-    },
 
-    {
-        prompt: "In JavaScript, which of the following is a logical operator?",
-        options: ["|", "&&", "%", "/"],
-        answer: "&&" 
-    },
+{
+    prompt: "How do you call a function?",
+    options: ["function[]", "function()", "#function", ".function"],
+    answer: "function()"
+},
 
-    {
-        prompt: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
-        options: ["method", "assignment operator", "variable", "string"],
-        answer: "variable"
-    }];
+
+{
+    prompt: "What is main in this example? (<main class= 'header'> ",
+    options: ["element", "class", "header", "parameter"],
+    answer: "element"
+},
+
+
+{
+    prompt: "In JavaScript, which of the following is a OR operator?",
+    options: ["||", "&&", "%", "/"],
+    answer: "||"
+},
+
+
+{
+    prompt: "A named element in a JavaScript program that is used to store information into a list is called: ",
+    options: ["duck", "number", "array", "boolean"],
+    answer: "array"
+}];
+
+
 
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#timer");
@@ -126,6 +133,8 @@ function checkForEnter(event) {
         saveHighscore();
     }
 }
+
+
 nameEl.onkeyup = checkForEnter;
 
 submitBtn.onclick = saveHighscore;
